1420c9b44abaef4a599f4667c36fb7b5d60f465e
jonyfight <38216633+jonyfight@users.noreply.github.com>
2018-08-29 14:46:03 +0800

    Add files via upload

---
+++ inception_resnet_v2/pre_train/val_filelist.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#  提取当前目录下所有文件及路径
+import os
+#import cv2
+
+if __name__ == "__main__":
+
+    file_train = '/data0/data/val_split'
+    f = open("./val_file.txt", "w")
+    with open('./class_file.txt') as f2:
+        classes = f2.readlines()
+    files = [w.strip() for w in classes]
+
+    for subfile in files:
+        num_img = len(os.listdir(os.path.join(file_train, subfile)))
+        print("{} images of {}".format(num_img, subfile))
+        for file in os.listdir(os.path.join(file_train, subfile)):
+            if file.endswith(('.jpg','.png','.jpeg','.JPG','.PNG','.JPEG')):
+                f.writelines(os.path.join(file_train, subfile, file) + " " + subfile +'\n')
+
+
+
+    f.close()
+
